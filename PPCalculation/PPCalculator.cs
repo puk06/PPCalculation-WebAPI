bf6a41d15695ab099bb1d970f5838f9eaf1a9818
Puk06 <86549420+puk06@users.noreply.github.com>
2025-09-07 01:44:58 +0900

    プロジェクト ファイルを追加します。

---
+++ PPCalculation/PPCalculator.cs
@@ -0,0 +1,11 @@
+using PPCalculationAPI.Models;
+
+namespace PPCalculationAPI.PPCalculation;
+
+internal class PPCalculator
+{
+    internal PPCalculator(CalculationArgs calculationArgs)
+    {
+
+    }
+}
